246df3b44bc27d3a858d3b9933643418e3ebab71
venkorls <715139533@qq.com>
2017-12-29 23:42:09 +0800

    add polyfill

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-
 import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
+import 'babel-polyfill'
 
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
